78151505744aa7f0544a0664ea3617602a5fc0b3
Bolu Femi <olufemioluyomi@outlook.com>
2022-04-22 16:03:04 +0100

    init

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,9 +1,10 @@
+import FeaturedInfo from "../../Components/featuredInfo/FeaturedInfo";
 import "./home.css"
 
 export default function Home() {
   return (
     <div className="home"> 
-      
+      <FeaturedInfo />
     </div>
   );
 }
