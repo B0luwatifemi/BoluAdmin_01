a7bccfc641965dda6be0370ead3d9ea71e1a96bf
Bolu Femi <olufemioluyomi@outlook.com>
2022-04-23 16:45:30 +0100

    init

--- src/Components/widgetLg/WidgetLg.jsx
+++ src/Components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,35 @@
 import "./widgetLg.css"
 
 export default function WidgetLg() {
+
+  const Button = ({type}) =>{
+    return <button className={"widgetLgButton" + type}>{type}</button>;
+  };
+
   return (
     <div className="widgetLg">
-      large
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="./Image/Bolu.jpg.jpg" 
+            alt="" 
+            className="widgetLgImage" 
+            />
+
+            <span className="widgetLgName">Bolu Tifemi</span>
+          </td>
+          <td className="widgetLgDate">23 April 2022</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus"><Button type="Approved"/></td>
+        </tr>
+      </table>
     </div>
   )
 }
