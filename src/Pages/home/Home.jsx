e3c15d51fce982b6a7b33378cef04e5f9c037190
Bolu Femi <olufemioluyomi@outlook.com>
2022-04-23 14:36:28 +0100

    init

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import Chart from "../../Components/charts/Chart";
 import FeaturedInfo from "../../Components/featuredInfo/FeaturedInfo";
-import "./home.css"
-
+import "./home.css";
+import {userData} from "../../dummyData"
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart />
+      <Chart data ={userData} title= "User Analytics" grid dataKey="Active User"/>
     </div>
   );
 }
---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 5000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 2000,
+    },
+    {
+      name: 'May',
+      "Active User": 1000,
+    },
+    {
+      name: 'Jun',
+      "Active User": 3500,
+    },
+    {
+      name: 'Jul',
+      "Active User": 4500,
+    },
+    {
+      name: 'Aug',
+      "Active User": 1000,
+    },
+    {
+      name: 'Sep',
+      "Active User": 2500,
+    },
+    {
+      name: 'Oct',
+      "Active User": 1200,
+    },
+    {
+      name: 'Nov',
+      "Active User": 4000,
+    },
+    {
+      name: 'Dec',
+      "Active User": 2000,
+    },
+
+  ];
